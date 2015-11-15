6185c6568a823454a8792f698095b1277dd0db59
Denis V. Meltsaykin <dmeltsaykin@mirantis.com>
2015-11-15 18:45:42 +0300

    some profiling

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 	"fmt"
 	"bufio"
-
+	"runtime/pprof"
+	"strings"
+	"os"
 )
 
 func main() {
+	fileprof, _ := os.Create("./profile_go")
+	pprof.StartCPUProfile(fileprof)
+	defer pprof.StopCPUProfile()
 	x, _ := http.Get("https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv")
 	outs := bufio.NewScanner(x.Body)
 	for outs.Scan() {
-		fmt.Printf("->>> %s\n", outs.Text())
+		if val := strings.Split(outs.Text(), ";"); len(val) > 2 {
+			//fmt.Printf(">>%q<<\n", val[2])
+		} else {
+			fmt.Printf("short: %q\n", val)
+		}
 	}
 }
