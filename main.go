9cda713fa15045b552f2a2f2cf624d5f9404e89e
Denis V. Meltsaykin <dmeltsaykin@mirantis.com>
2015-11-29 17:38:24 +0300

    [WIP] workaround some colons in urls
    escape them with "\"

--- main.go
+++ main.go
@@ -28,12 +28,18 @@ func url_parse(raw []string, urls map[string]bool, ips map[string]bool) bool {
 			return true
 		} else {
 			for _, v := range strings.Split(raw[2], " | ") {
+				v := normalize_url(v)
 				urls[v] = true
 			}
 			return true
 		}
 	}
 	return false
+}
+
+func normalize_url(src string) string {
+	//takes string and escapes ":" in it
+	return src[:5] + strings.Replace(src[5:], ":", "\\:", -1)
 }
 
 func main() {
