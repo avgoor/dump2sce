187ac844881b8364aba08e6655ef2b0d41c22201
Denis V. Meltsaykin <dmeltsaykin@mirantis.com>
2015-12-08 16:52:11 +0300

    [wip] changed format of output
    Get rid of "http://"

--- utils/urls.go
+++ utils/urls.go
@@ -3,6 +3,7 @@ package utils
 import "strings"
 
 func Url_parse(raw []string, urls map[string]bool, ips map[string]bool) bool {
+	// TODO: split strings before checking HTTPS!
 	if len(raw) < 3 { //short strings is a problem
 		return false
 	}
@@ -26,5 +27,5 @@ func Url_parse(raw []string, urls map[string]bool, ips map[string]bool) bool {
 
 func normalize_url(src string) string {
 	//takes string and escapes ":" in it
-	return src[:5] + strings.Replace(src[5:], ":", "\\:", -1)
+	return strings.Replace(src[7:], ":", "\\:", -1)
 }
